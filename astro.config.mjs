18b39a5e1f82e91d7bbe622281f38b3912798baf
Andy <github@me.andymonroe.dev>
2024-10-02 17:23:18 +0100

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-
+  output: 'static',
   prefetch: {
     defaultStrategy: 'viewport'
   },
