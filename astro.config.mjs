fbb41f44786439325a7851a9096d16103ad2657e
Andy <github@me.andymonroe.dev>
2024-10-02 17:17:20 +0100

    Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify(),
+
   prefetch: {
     defaultStrategy: 'viewport'
-  }
+  },
+
+  adapter: cloudflare()
 });
